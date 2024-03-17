03676ef63b9aee6cbbe6b161e98f32d707865d64
Vitalii Shapovalov <martyit@ukr.net>
2024-03-17 12:31:53 +0100

    messages tab, little js for eliminate conflicts between tabs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 window.onload = function(){
 	const profileBtn = document.getElementById('profile-btn')
+	const messagesBlock = document.getElementById('messages')
+	
+	function checkMessages(){
+		if(messagesBlock.classList.contains('active')){
+			messagesBlock.style.display = 'block'
+		} else{
+			messagesBlock.style.display = 'none'
+		}
+	}	
+	
 	profileBtn.onclick = function(){
 		const profile = document.getElementById('profile-tab')
-
 		const profileTab = new bootstrap.Tab(profile)
 		profileTab.show()
 	}
+	checkMessages()
+	document.addEventListener('shown.bs.tab', function (event) {
+		if (event.target.id === 'messages-tab') {
+			checkMessages()
+		} else{
+			checkMessages()
+		}
+	})
 }
